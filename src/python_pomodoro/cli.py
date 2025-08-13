29fba35a0d405855258326468f935511478a928e
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-08-13 10:53:07 -0600

    added initial command scaffold for other intiall commands

--- src/python_pomodoro/cli.py
+++ src/python_pomodoro/cli.py
@@ -9,3 +9,33 @@ def cli():
 @cli.command("start")
 def start():
     click.echo("Starting timer. (Not yet implemented)")
+
+    # Check for arguments (to determin focus and break timer duration. Anything else???)
+    # Check for config file
+    # If no config file, create one
+    #
+
+
+@cli.command("pause")
+def pause():
+    click.echo("Pausing timer. (not yet implemented)")
+
+
+@cli.command("resume")
+def resume():
+    click.echo("Resuming timer. (not yet implemented)")
+
+
+@cli.command("cancel")
+def stop():
+    click.echo("Canceling timer. (not yet implemented)")
+
+
+@cli.command("status")
+def status():
+    click.echo("Getting timer status. (not yet implemented)")
+
+
+@cli.command("config")
+def config():
+    click.echo("Setting config. (not yet implemented)")
